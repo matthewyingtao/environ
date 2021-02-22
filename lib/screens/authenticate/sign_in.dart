7c78070dd6a85ffd015e18f1c416918b6445fcb6
mkomko942 <s134584@gapps.dilworth.school.nz>
2021-02-22 16:48:11 +1300

    first commit

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,40 @@
+import 'package:brewcrew/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text("Sign In to Brew Crew"),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          child: Text("Sign in anon"),
+          onPressed: () async {
+            await Firebase.initializeApp();
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error signing in');
+            } else {
+              print('signed in');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:brewcrew/screens/authenticate/authenticate.dart';
+import 'package:brewcrew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
