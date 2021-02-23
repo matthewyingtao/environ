0d6b44ed60a22bde5cc8621d16ff7c16e4615237
mkomko942 <goatatia@gmail.com>
2021-02-23 15:26:14 +1300

    disable null sound safety

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,5 +1,5 @@
 class UserModel {
-  final String? uid;
+  final String uid;
 
   UserModel({this.uid});
 }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -4,14 +4,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  UserModel? _userFromUserCredential(User user) {
+  UserModel _userFromUserCredential(User user) {
     return UserModel(uid: user.uid);
+  }
+
+  Stream<UserModel> get user {
+    return _auth
+        .authStateChanges()
+        .map((User user) => _userFromUserCredential(user));
   }
 
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
-      User? user = result.user;
+      User user = result.user;
       if (user != null) {
         return _userFromUserCredential(user);
       } else {
