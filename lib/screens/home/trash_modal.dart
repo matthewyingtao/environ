7586fd60332a3ab19d5d5e9783f81da12474ae85
Matthew Tao <goatatia@gmail.com>
2021-07-18 11:13:18 +1200

    implemented new trash category images added border around trash category image

--- lib/screens/home/trash_modal.dart
+++ lib/screens/home/trash_modal.dart
@@ -6,9 +6,30 @@ class TrashModal extends StatelessWidget {
   final List data;
   const TrashModal({this.data});
 
+  String _getTrashImage(trashName) {
+    switch (trashName) {
+      case "trash":
+        return "assets/trash_category/trash.jpg";
+      case "plastic":
+        return "assets/trash_category/plastic.jpg";
+      case "metal":
+        return "assets/trash_category/metal.jpg";
+      case "paper-cardboard":
+        return "assets/trash_category/cardboard.jpg";
+      case "colored-glass":
+        return "assets/trash_category/glass.jpg";
+      case "clear-glass":
+        return "assets/trash_category/glass.jpg";
+
+      default:
+        return "assets/trash_category/trash.jpg";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final bool isRecyclable = this.data[0]['label'] == "trash" ? false : true;
+    final String trashImage = _getTrashImage(this.data[0]['label']);
 
     return Container(
       decoration: BoxDecoration(
@@ -19,7 +40,7 @@ class TrashModal extends StatelessWidget {
         color: isRecyclable ? themeBlue : themeRed,
       ),
       child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
         children: [
           Text(
             isRecyclable ? "Recyclable" : "Non-Recyclable",
@@ -31,15 +52,20 @@ class TrashModal extends StatelessWidget {
             style: Theme.of(context).textTheme.headline5,
             textAlign: TextAlign.center,
           ),
-          SizedBox(height: 32.0),
+          SizedBox(height: 16.0),
           AspectRatio(
             aspectRatio: 1,
             child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16.0),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2.0,
+                ),
                 image: DecorationImage(
                   fit: BoxFit.cover,
-                  image: AssetImage("assets/cardboard.jpg"),
+                  image: AssetImage(trashImage),
                 ),
               ),
             ),
