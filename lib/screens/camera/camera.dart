328e8d098566648e60d370a54384dd2ede0eb303
Matthew Tao <goatatia@gmail.com>
2021-07-18 17:15:00 +1200

    abstracted shutter button bar into seperate file

--- lib/screens/camera/camera.dart
+++ lib/screens/camera/camera.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:environ/screens/camera/model.dart';
-import 'package:environ/shared/constants.dart';
+import 'package:environ/screens/camera/shutter_button_bar.dart';
 import 'package:environ/shared/loading.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
@@ -73,6 +73,29 @@ class TakePictureScreenState extends State<TakePictureScreen>
     return firstCamera;
   }
 
+  void takePhoto() async {
+    // ensure that the photo taken won't use flash because flash freezes the camera
+    _controller.setFlashMode(FlashMode.off);
+    final image = await _controller.takePicture();
+
+    Model _model = Model(path: image.path);
+
+    setState(() {
+      _isModelRunning = true;
+    });
+
+    try {
+      List result = await _model.useModel();
+      widget.changeData(result);
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      _isModelRunning = false;
+    });
+    await _model.closeModel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -92,55 +115,9 @@ class TakePictureScreenState extends State<TakePictureScreen>
               // makes sure that the shutter button bar is pushed to the bottom
               Spacer(),
               // shutter button bar
-              Container(
-                color: Colors.black45,
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: Column(
-                  children: [
-                    RawMaterialButton(
-                      elevation: 4.0,
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: themeGreen,
-                        size: 32.0,
-                      ),
-                      fillColor: Colors.white,
-                      padding: EdgeInsets.all(12.0),
-                      shape: CircleBorder(
-                        side: BorderSide(
-                          color: themeGreen,
-                          width: 4,
-                        ),
-                      ),
-
-                      // runs the model and sets the result as a variable
-                      onPressed: () async {
-                        // ensure that the photo taken won't use flash
-                        _controller.setFlashMode(FlashMode.off);
-                        final image = await _controller.takePicture();
-
-                        Model _model = Model(path: image.path);
-
-                        setState(() {
-                          _isModelRunning = true;
-                        });
-
-                        try {
-                          List result = await _model.useModel();
-                          widget.changeData(result);
-                        } catch (e) {
-                          print(e);
-                        }
-                        setState(() {
-                          _isModelRunning = false;
-                        });
-                        await _model.closeModel();
-                      },
-                    ),
-                  ],
-                ),
-              ),
+              ShutterButtonBar(
+                onPressed: takePhoto,
+              )
             ],
           ),
         ),
---
+++ lib/screens/camera/shutter_button_bar.dart
@@ -0,0 +1,40 @@
+import 'package:environ/shared/constants.dart';
+import 'package:flutter/material.dart';
+
+class ShutterButtonBar extends StatelessWidget {
+  final Function onPressed;
+
+  ShutterButtonBar({this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black45,
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Column(
+        children: [
+          RawMaterialButton(
+            elevation: 4.0,
+            child: Icon(
+              Icons.camera_alt,
+              color: themeGreen,
+              size: 36.0,
+            ),
+            fillColor: Colors.white,
+            padding: EdgeInsets.all(12.0),
+            shape: CircleBorder(
+              side: BorderSide(
+                color: themeGreen,
+                width: 4,
+              ),
+            ),
+
+            // runs the model and sets the result as a variable
+            onPressed: this.onPressed,
+          ),
+        ],
+      ),
+    );
+  }
+}
