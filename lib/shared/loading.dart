f312e987e0878d384a09ad10e887d7678ae4fdd0
Matthew Tao <goatatia@gmail.com>
2021-07-21 14:38:08 +1200

    added optional argument to  Loading
    you can make the background transparent

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
+  final bool transparent;
+  Loading({this.transparent = false});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: themeGreen,
+      color: this.transparent ? Colors.black54 : themeGreen,
       child: Center(
         child: SpinKitFoldingCube(
           color: Colors.white,
