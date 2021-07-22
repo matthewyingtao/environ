5bb659eca07b0566807a82708e14499d3c510f4f
Matthew Tao <goatatia@gmail.com>
2021-07-22 20:18:45 +1200

    make ReportCard non-nullable

--- lib/shared/components/report_card.dart
+++ lib/shared/components/report_card.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ReportCard extends StatelessWidget {
-  ReportCard({this.icon, this.body, this.main});
+  final IconData icon;
+  final String body;
+  final bool main;
 
-  final IconData? icon;
-  final String? body;
-  final bool? main;
+  ReportCard({
+    required this.icon,
+    required this.body,
+    required this.main,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 8.0),
-      constraints: main!
+      constraints: main
           ? BoxConstraints(minWidth: 100, maxWidth: 200)
           : BoxConstraints(minWidth: 80, maxWidth: 120),
       decoration: BoxDecoration(
@@ -29,12 +33,12 @@ class ReportCard extends StatelessWidget {
         children: [
           Icon(
             this.icon,
-            size: main! ? 48.0 : 32.0,
+            size: main ? 48.0 : 32.0,
             color: Colors.white,
           ),
           SizedBox(height: 8.0),
           Text(
-            this.body!,
+            this.body,
             textAlign: TextAlign.center,
           )
         ],
