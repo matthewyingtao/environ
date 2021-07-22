4823b528ab5274c4f67c60ca01e1d291a1283a98
Matthew Tao <goatatia@gmail.com>
2021-07-22 20:41:30 +1200

    Moved Listview horizontal padding to LessonCard
    this stops LessonCard's shadows from clipping

--- lib/screens/home/lessons.dart
+++ lib/screens/home/lessons.dart
@@ -4,54 +4,53 @@ import 'package:flutter/material.dart';
 class Lessons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: ListView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          SizedBox(height: 24.0),
-          Text(
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        SizedBox(height: 24.0),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
             "Lessons",
             style: Theme.of(context).textTheme.headline3,
           ),
-          SizedBox(height: 24.0),
-          LessonCard(
-            heading: "Paper & Cardboard",
-            body:
-                "This includes: \n• Newspapers and magazines \n• Corrugated cardboard \n• Office paper",
-            icon: Icons.description_rounded,
-            imagePath: "assets/trash_category/cardboard.jpg",
-          ),
-          LessonCard(
-            heading: "Glass",
-            body:
-                "This includes: \n• Bottles, jars and containers \n• Clear and colored Glass",
-            icon: Icons.liquor_rounded,
-            imagePath: "assets/trash_category/glass.jpg",
-          ),
-          LessonCard(
-            heading: "Metal",
-            body: "This includes: \n• Aluminium and tin cans \n• Aerosols",
-            icon: Icons.iron_rounded,
-            imagePath: "assets/trash_category/metal.jpg",
-          ),
-          LessonCard(
-            heading: "Plastic",
-            body:
-                "This includes: \n• Milk and drink bottles \n• Plastic shrink wrap",
-            icon: Icons.casino_rounded,
-            imagePath: "assets/trash_category/plastic.jpg",
-          ),
-          LessonCard(
-            heading: "Reducing Waste",
-            body:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            icon: Icons.delete,
-            imagePath: "assets/trash_category/trash.jpg",
-          ),
-          SizedBox(height: 24.0),
-        ],
-      ),
+        ),
+        LessonCard(
+          heading: "Paper & Cardboard",
+          body:
+              "This includes: \n• Newspapers and magazines \n• Corrugated cardboard \n• Office paper",
+          icon: Icons.description_rounded,
+          imagePath: "assets/trash_category/cardboard.jpg",
+        ),
+        LessonCard(
+          heading: "Glass",
+          body:
+              "This includes: \n• Bottles, jars and containers \n• Clear and colored Glass",
+          icon: Icons.liquor_rounded,
+          imagePath: "assets/trash_category/glass.jpg",
+        ),
+        LessonCard(
+          heading: "Metal",
+          body: "This includes: \n• Aluminium and tin cans \n• Aerosols",
+          icon: Icons.iron_rounded,
+          imagePath: "assets/trash_category/metal.jpg",
+        ),
+        LessonCard(
+          heading: "Plastic",
+          body:
+              "This includes: \n• Milk and drink bottles \n• Plastic shrink wrap",
+          icon: Icons.casino_rounded,
+          imagePath: "assets/trash_category/plastic.jpg",
+        ),
+        LessonCard(
+          heading: "Reducing Waste",
+          body:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          icon: Icons.delete,
+          imagePath: "assets/trash_category/trash.jpg",
+        ),
+        SizedBox(height: 24.0),
+      ],
     );
   }
 }
--- lib/shared/components/lesson_card.dart
+++ lib/shared/components/lesson_card.dart
@@ -15,62 +15,58 @@ class LessonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.white,
-              width: 2,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.5),
-                blurRadius: 6,
-                offset: Offset(0, 6),
-              ),
-            ],
-            image: DecorationImage(
-              image: AssetImage(this.imagePath),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.4), BlendMode.dstATop),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                this.heading,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  Expanded(
-                    child: Text(
-                      this.body,
-                      style: Theme.of(context).textTheme.bodyText2,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 16),
-                    child: Icon(
-                      this.icon,
-                      color: Colors.white,
-                      size: 48,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+    return Container(
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.white,
+          width: 2,
         ),
-        SizedBox(height: 24.0),
-      ],
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.5),
+            blurRadius: 6,
+            offset: Offset(0, 6),
+          ),
+        ],
+        image: DecorationImage(
+          image: AssetImage(this.imagePath),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.4), BlendMode.dstATop),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            this.heading,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  this.body,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 16),
+                child: Icon(
+                  this.icon,
+                  color: Colors.white,
+                  size: 48,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
